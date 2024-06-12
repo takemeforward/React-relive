68228fee0836953f81aa8442953e2bf18d500dfa
Vishal Kumar <108937541+takemeforward@users.noreply.github.com>
2024-06-12 05:21:44 +0000

    .

---
+++ 08_react_project1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/App.css'
+export default function Header() {
+    return (
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>
+    )
+}
