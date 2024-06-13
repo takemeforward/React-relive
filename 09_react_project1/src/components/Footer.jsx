022e812e76a2891f65b47b1febcfaa4f6e9c09b4
Vishal Kumar <108937541+takemeforward@users.noreply.github.com>
2024-06-13 06:36:19 +0000

    before-contact

---
+++ 09_react_project1/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <h1>TechyStar</h1>
+                <p>@all rights reserved 2024</p>
+            </div>
+            <div>
+                <h5>Follow Us</h5>
+                <div>
+                    <a href="youtube.com" target='blank'>Youtube</a>
+                    <a href="youtube.com" target='blank'>Instagram</a>
+                    <a href="youtube.com" target='blank'>GitHub</a>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
