0a82d9d226995f552be69ddb08d4b779c8782fe5
Vishal Kumar <108937541+takemeforward@users.noreply.github.com>
2024-06-22 18:35:22 +0000

    start Redux toolkit

--- 08_react_project1/src/components/Home.jsx
+++ 08_react_project1/src/components/Home.jsx
@@ -1,34 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import Coin from '../Coin';
-import Loader from './Loader';
-import axios from 'axios'
-export default function Home() {
-    const [coinData, setCoinData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const fetchAllCoins = async () => {
-            const { data } = await axios.get(
-                'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&per_page=520'
-            );
-            console.log(data);
-            setCoinData(data);
-            setLoading(false);
-        }
-        fetchAllCoins();
+import React from 'react'
 
-    })
+export default function Home() {
+
 
     return (
         <div className='container'>
-            {
-                loading ? <Loader /> : coinData.map((i) => (
-                    <Coin name={i.name}
-                        symbol={i.symbol}
-                        imgSrc={i.image}
-                        price={i.current_price}
-                        key={i.id} />
-                ))
-            }
+            Home
         </div>
     )
 }
